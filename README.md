e5bac489cd525134dcea61882b125d4b17b7c397
temuller <t.e.muller-bravo@ice.csic.es>
2023-05-22 16:50:56 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# svo_api
+# SVO API
+API to download [Spanish Virtual Observatory](http://svo2.cab.inta-csic.es/theory/fps/index.php?mode=browse) filters.
+
+[![repo](https://img.shields.io/badge/GitHub-temuller%2Fsvo_api-blue.svg?style=flat)](https://github.com/temuller/svo_api)
+[![license](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](https://github.com/temuller/svo_api/blob/master/LICENSE)
+![Python Version](https://img.shields.io/badge/Python-3.9%2B-blue)
+[![PyPI](https://img.shields.io/pypi/v/wiserep_api?label=PyPI&logo=pypi&logoColor=white)](https://pypi.org/project/svo_api/)
+
+## Installation
+
+It is recommended to install ``svo_api`` from pip:
+
+```python
+pip install svo_api
+```
+or it can be installed from source in the usual way.
+
+## Usage Example
+
+You can obtain the list of available facilities in SVO in one command:
+
+```python
+get_facilities()
+```
+
+Many facilities can have multiple filters sets, e.g. for different instruments and telescopes. These can be obtained as follows:
+
+```python
+facility = 'Paranal'
+get_filters_sets(facility)
+```
+
+To download the filters, run the following command:
+
+```python
+facility = 'Paranal'
+filter_sets = 'HAWKI'  # if not specified, download all the filters sets
+download_filter(facility, filter_sets)
+```
+
+## Contributing
+To contribute, either open an issue or send a pull request (prefered option). You can also contact me directly (check my profile: https://github.com/temuller).
+
+## Citing SVO API
+
+If you make use of this code, please cite it:
+
+```code
+To be added
+```
---
+++ svo_api/svo.py
@@ -0,0 +1,159 @@
+import os
+import requests
+
+def get_response(url):
+    """Obtains the response from a given Wiserep URL.
+    
+    Parameters
+    ----------
+    url: str
+        Wiserep URL.
+        
+    Returns
+    -------
+    response: requests.Response
+        Response object.
+    """
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response
+    else:
+        return None
+    
+def get_facilities():
+    """Gets a list of all the observational facilities in SVO.
+
+    Returns
+    -------
+    facilities: list
+        Observational facilities.
+    """
+    # SVO main filters' webpage
+    svo_url = 'http://svo2.cab.inta-csic.es/theory/fps/fps.php'
+    response = get_response(svo_url)
+    
+    # get where the observational facilities are listed
+    split_text = response.text.split('<PARAM')
+    for section in split_text:
+        if "INPUT:Facility" in section:
+            facilities_section = section
+         
+        
+    # obtain list of facilities
+    facilities = []
+    split_section = facilities_section.split('<OPTION')[1:]
+    for line in split_section:
+        facility = line.split('"')[1]
+        facilities.append(facility)
+        
+    return facilities
+
+def get_filters_sets(facility):
+    """Gets a list of filters sets for a given facility.
+    
+    Parameters
+    ----------
+    facility: str
+        Name of the observational facility.
+
+    Returns
+    -------
+    filters_sets: list
+        Filters sets.
+    """
+    # get the facility's SVO URL
+    search_url = 'http://svo2.cab.inta-csic.es/theory/fps/index.php?'
+    facility_url = search_url + f'mode=browse&gname={facility}&asttype='
+    response = get_response(facility_url)
+    
+    # get list of filters sets
+    filters_sets = []
+    filters_split = response.text.split(f'gname={facility}&gname2=')[1:]
+    for line in filters_split:
+        if len(line.split('&asttype=')) > 1:
+            filter_set = line.split('&asttype=')[0]
+            filters_sets.append(filter_set)
+            
+    if len(filters_sets)==0:
+        filters_sets = [facility]
+
+    return filters_sets
+
+def get_filters(facility, filters_sets=None, verbose=False):
+    """Gets a list of filters for a given facility.
+    
+    Parameters
+    ----------
+    facility: str
+        Name of the observational facility.
+    filters_sets: str or list, default ``None``
+        Name of the filters set(s). If not given, gets all the filters.
+    verbose: bool, default ``False``
+        If ``True``, the filter_set+filter name is printed.
+        
+    Returns
+    -------
+    filters: list
+        Filters for the facility.
+    """
+    search_url = 'http://svo2.cab.inta-csic.es/theory/fps/index.php?'
+    if filters_sets is None:
+        filters_sets = get_filters_sets(facility)
+    else:
+        if isinstance(filters_sets, str) is True:
+            filters_sets = [filters_sets]
+        valid_filters_sets = get_filters_sets(facility)
+        for filter_set in filters_sets:
+            assert filter_set in valid_filters_sets, f"Not a valid filter set ({filter_set}): {valid_filters_sets}"
+    filters = []
+    
+    for filter_set in filters_sets:
+        # get the facility + filters set's SVO URL
+        filters_url = search_url + f'mode=browse&gname={facility}&gname2={filter_set}&asttype='
+        response = get_response(filters_url)
+
+        # get list of filters
+        filters_split = response.text.split(f'gname={facility}&gname2={filter_set}')
+        for line in filters_split:
+            if line.startswith('#filter'):
+                line_split = line.split('/')
+                filt = line_split[1][:-1]
+                filters.append(filt)
+                
+                if verbose is True:
+                    print(f'{filter_set}/{filt}')
+            
+    return filters
+
+def download_filter(facility, filter_sets=None):
+    """Downloads a filter file in ascii format form the SVO website.
+    
+    The output file name has the same name as the filter, with '.dat'
+    extension.
+    
+    Parameters
+    ----------
+    facility: str
+        Name of the observational facility.
+    filter_sets: str or list, default ``None``
+        Name of the filters set(s). If not given, gets all the filters.
+    """
+    filters = get_filters(facility, filter_sets)
+    
+    for filt in filters:
+        # get-data URL
+        getdata_url = 'http://svo2.cab.inta-csic.es/theory/fps/getdata.php?'
+        filter_url = getdata_url + f'format=ascii&id={facility}/{filt}'
+
+        # output file
+        response = get_response(filter_url)
+        outfile = os.path.join(facility, f'{filt}.dat')
+
+        # check if the directory exists
+        if not os.path.isdir(facility):
+            os.mkdir(facility)
+
+        # save data 
+        with open(outfile, 'wb') as file:
+            file.write(response.content)
